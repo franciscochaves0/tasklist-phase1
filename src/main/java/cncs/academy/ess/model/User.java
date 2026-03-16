a3e34c953bee1dc61bec629c37c87b4fc212d05f
Francisco Chaves <francisconunogchaves@gmail.com>
2026-03-16 17:44:47 +0000

    first commit

--- src/main/java/cncs/academy/ess/model/User.java
+++ src/main/java/cncs/academy/ess/model/User.java
@@ -4,6 +4,7 @@ public class User {
     private int id;
     private String username;
     private String password;
+
     public User(int id, String username, String password) {
         this.id = id;
         this.username = username;
